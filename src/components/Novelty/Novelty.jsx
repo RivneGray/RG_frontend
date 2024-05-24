d288d9da6ed2c9a3e054683cd3303f2bbb2d8ac5
sssBless <babuleh11@gmail.com>
2024-05-24 23:32:34 +0300

    fix some problems with product card page and novelty

--- src/components/Novelty/Novelty.jsx
+++ src/components/Novelty/Novelty.jsx
@@ -1,29 +1,30 @@
-import { ProductCard } from "../ProductCard/ProductCard";
+import { ProductCard } from '../ProductCard/ProductCard';
 import styles from './Novelty.module.css';
-import { useQuery } from "@tanstack/react-query";
-import { boardgameApi } from "../../api/boardgameAPI";
-import { Loader } from "../Loader/Loader";
+import { useQuery } from '@tanstack/react-query';
+import { boardgameApi } from '../../api/boardgameAPI';
+import { Loader } from '../Loader/Loader';
 
 export const Novelty = () => {
-  const {
-    data,
-    isLoading
-  } = useQuery({
-    queryFn: () => boardgameApi.getNewestList()
+  const { data, isLoading } = useQuery({
+    queryFn: () => boardgameApi.getNewestList(),
   });
-  if (isLoading) return <Loader/>;
+  if (isLoading || !Array.isArray(data)) return <Loader />;
+
   return (
     <section>
       <h1>НОВИНКА</h1>
       <div className={styles.listNew}>
-        {data.map(prod => <ProductCard 
-        productName={prod.productName}
-        productNameInEnglish={prod.productNameInEnglish}
-        productPrice={prod.productPrice}
-        productQuantityInStock={prod.productQuantityInStock}
-        productImageURL={prod.productImageURL}
-        id={prod.id}
-        key={prod.id}/>)}
+        {data.map((prod) => (
+          <ProductCard
+            productName={prod.productName}
+            productNameInEnglish={prod.productNameInEnglish}
+            productPrice={prod.productPrice}
+            productQuantityInStock={prod.productQuantityInStock}
+            productImageURL={prod.productImageURL}
+            id={prod.id}
+            key={prod.id}
+          />
+        ))}
       </div>
     </section>
   );
